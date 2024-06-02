aa4d78a2c6b459239b21f6f3792000cd826b7c07
JostonSaldanha <jostonsaldanha0@gmail.com>
2024-06-02 11:17:37 +0530

    model trained

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,25 +1,25 @@
-import os
-import sys
-import mlflow
-import mlflow.sklearn
-import numpy as np
-import pickle
-from src.utils.utils import load_object
-from urllib.parse import urlparse
-from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
-from src.logger.logging import logging
-from src.exception. exception import customexception
+# import os
+# import sys
+# import mlflow
+# import mlflow.sklearn
+# import numpy as np
+# import pickle
+# from src.utils.utils import load_object
+# from urllib.parse import urlparse
+# from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+# from src.logger.logging import logging
+# from src.exception. exception import customexception
 
 
-@dataclass
-class ModelEvaluationConfig:
-    pass
+# @dataclass
+# class ModelEvaluationConfig:
+#     pass
 
-class ModelEvaluation:
-    def __init__ (self):
-        pass
-    def get_model_evaluation(self):
-        try:
-            pass
-        except Exception as e:
-            raise customexception(e,sys)
+# class ModelEvaluation:
+#     def __init__ (self):
+#         pass
+#     def get_model_evaluation(self):
+#         try:
+#             pass
+#         except Exception as e:
+#             raise customexception(e,sys)
